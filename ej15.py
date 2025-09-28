34babb6df03b8ab471af2957c6b19d258ebf7a47
DavidAnzola05 <davidanzola014@gmail.com>
2025-09-28 15:55:07 -0500

    Update ej15.py

--- ej15.py
+++ ej15.py
@@ -2,32 +2,14 @@ import time
 
 class Semaforo:
     def __init__(self):
-        self.estado = "rojo"   
-        self.tiempos = {       
-            "rojo": 5,
-            "verde": 4,
-            "amarillo": 2
-        }
-
-    def mostrar_estado(self):
-        print(f"El semáforo está en {self.estado.upper()}")
-
-    def cambiar_estado(self):
-        if self.estado == "rojo":
-            self.estado = "amarillo"
-        elif self.estado == "amarillo":
-            self.estado = "verde"
-        elif self.estado == "verde":
-            self.estado = "rojo"
-
+        self.estados = [("rojo", 5), ("amarillo", 2), ("verde", 4)]
+        self.i = 0
     def iniciar(self, ciclos=3):
-        """Ejecuta el semáforo por un número de ciclos"""
-        for _ in range(ciclos):
-            self.mostrar_estado()
-            time.sleep(self.tiempos[self.estado])  
-            self.cambiar_estado()
+        for _ in range(ciclos*len(self.estados)):
+            e, t = self.estados[self.i]
+            print(f"El semáforo está en {e.upper()}")
+            time.sleep(t)
+            self.i = (self.i+1) % len(self.estados)
 
-semaforo = Semaforo()
-semaforo.iniciar()  
-
-print("Simulación del semáforo finalizada.")
+Semaforo().iniciar()
+print("Simulación finalizada.")
