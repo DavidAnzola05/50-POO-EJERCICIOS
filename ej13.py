c1dba351514a6c05aedc82121107bde1448705c0
DavidAnzola05 <davidanzola014@gmail.com>
2025-09-28 15:09:19 -0500

    ej13.py

--- ej13.py
+++ ej13.py
@@ -1,28 +1,10 @@
-class temperatura:
-    def __init__(self, celsius):
-        self.celsius = celsius
+class Temperatura:
+    def __init__(self, c): self.c = c
+    def f(self): return self.c*9/5+32
+    def k(self): return self.c+273.15
 
-    def to_fahrenheit(self):
-        return (self.celsius * 9/5) + 32
+c = float(input("Ingrese la temperatura en Celsius: "))
+t = Temperatura(c)
 
-    def to_kelvin(self):
-        return self.celsius + 273.15
-
-    def from_fahrenheit(self, fahrenheit):
-        self.celsius = (fahrenheit - 32) * 5/9
-        return self.celsius
-
-    def from_kelvin(self, kelvin):
-        self.celsius = kelvin - 273.15
-        return self.celsius
-    
-temp = input("Ingrese la temperatura en Celsius: ")
-temp = temperatura(float(temp))
-print("Que conversion te gustaria hacer?: ")
-print("1. Celsius a Fahrenheit")
-print("2. Celsius a Kelvin")
-opcion = input("Ingrese la opcion (1/2): ")
-if opcion == '1':
-    print(f"{temp.celsius}°C son {temp.to_fahrenheit()}°F")
-elif opcion == '2':
-    print(f"{temp.celsius}°C son {temp.to_kelvin()}K")
+op = input("1. Celsius a Fahrenheit\n2. Celsius a Kelvin\nOpción: ")
+print(f"{t.c}°C son {t.f()}°F") if op=="1" else print(f"{t.c}°C son {t.k()}K")
