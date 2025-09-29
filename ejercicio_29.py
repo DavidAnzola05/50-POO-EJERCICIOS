3ba99f949020cc03e5687973a5a42cab3503d482
DavidAnzola05 <davidanzola014@gmail.com>
2025-09-28 20:55:08 -0500

    Update ejercicio_29.py

--- ejercicio_29.py
+++ ejercicio_29.py
@@ -1,12 +1,11 @@
-class Clase29A:
-    def __init__(self, nombre):
-        self.nombre = nombre
+from datetime import datetime
+class Recurso: 
+    def __init__(s,n): s.n, s.disp = n, True
+class Usuario: 
+    def __init__(s,n): s.n, s.res = n,[]
+class Reserva: 
+    def __init__(s,r,u): s.r,s.u,s.f=r,u,datetime.now(); r.disp=False; u.res.append(s)
 
-class Clase29B:
-    def __init__(self, dato):
-        self.dato = dato
-
-if __name__ == "__main__":
-    a = Clase29A("EjemploA")
-    b = Clase29B("EjemploB")
-    print(a.nombre, b.dato)
+r,u=Recurso("Sala A"),Usuario("Ana")
+res=Reserva(r,u)
+print(f"{u.n} reservó {r.n} el {res.f:%Y-%m-%d %H:%M}")
