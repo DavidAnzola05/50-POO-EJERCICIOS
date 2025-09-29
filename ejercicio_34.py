8eb9a06d9be2e21e305cb3536a3bdd11bbdfd5eb
DavidAnzola05 <davidanzola014@gmail.com>
2025-09-28 21:32:52 -0500

    Update ejercicio_34.py

--- ejercicio_34.py
+++ ejercicio_34.py
@@ -1,12 +1,18 @@
-class Clase34A:
-    def __init__(self, nombre):
-        self.nombre = nombre
+class Profesor: 
+    def __init__(self,n,i): self.nombre, self.instrumento = n,i
 
-class Clase34B:
-    def __init__(self, dato):
-        self.dato = dato
+class Estudiante: 
+    def __init__(self,n,i): self.nombre,self.instrumento,self.progreso=n,i,0
+    def avanzar(self,p): self.progreso=min(100,self.progreso+p)
 
-if __name__ == "__main__":
-    a = Clase34A("EjemploA")
-    b = Clase34B("EjemploB")
-    print(a.nombre, b.dato)
+class Clase: 
+    def __init__(self,n,prof): self.nombre,self.profesor,self.estudiantes=n,prof,[]
+    def agregar(self,e): self.estudiantes.append(e)
+
+# Ejemplo
+p=Profesor("Laura","Piano")
+e=Estudiante("Ana","Piano")
+c=Clase("Piano Básico",p)
+c.agregar(e)
+e.avanzar(20)
+print(e.nombre,e.progreso)
