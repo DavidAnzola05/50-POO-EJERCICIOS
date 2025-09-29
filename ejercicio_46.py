6904425ddfaccd3725e6fe887c19e5714ee71865
DavidAnzola05 <davidanzola014@gmail.com>
2025-09-29 00:38:31 -0500

    Update ejercicio_46.py

--- ejercicio_46.py
+++ ejercicio_46.py
@@ -1,12 +1,18 @@
 class Clase46A:
-    def __init__(self, nombre):
-        self.nombre = nombre
+class Freelancer: 
+    def __init__(self,n): self.nombre=n; self.calificaciones=[]
+    def calificar(self,punt): self.calificaciones.append(punt)
+class Cliente: 
+    def __init__(self,n): self.nombre=n
+class Proyecto: 
+    def __init__(self,t,cliente): self.titulo=t; self.cliente=cliente; self.propuestas=[]
+    def agregar_propuesta(self,p): self.propuestas.append(p)
+class Propuesta: 
+    def __init__(self,freelancer,precio): self.freelancer=freelancer; self.precio=precio
 
-class Clase46B:
-    def __init__(self, dato):
-        self.dato = dato
-
-if __name__ == "__main__":
-    a = Clase46A("EjemploA")
-    b = Clase46B("EjemploB")
-    print(a.nombre, b.dato)
+# Ejemplo
+f=Freelancer("Ana"); c=Cliente("EmpresaX")
+p=Proyecto("Web",c); prop=Propuesta(f,500)
+p.agregar_propuesta(prop)
+f.calificar(5)
+print(f.nombre, f.calificaciones, p.titulo, [(pr.freelancer.nombre,pr.precio) for pr in p.propuestas])
