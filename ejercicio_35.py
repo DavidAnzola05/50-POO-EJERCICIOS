caf5026bfe082ebe039253a2e9a9949e1cda9439
DavidAnzola05 <davidanzola014@gmail.com>
2025-09-28 23:25:21 -0500

    Update ejercicio_35.py

--- ejercicio_35.py
+++ ejercicio_35.py
@@ -1,12 +1,18 @@
-class Clase35A:
-    def __init__(self, nombre):
-        self.nombre = nombre
+class Candidato: 
+    def __init__(self,n): self.nombre,self.votos=n,0
+class Votante: 
+    def __init__(self,n): self.nombre,self.voto=n,None
+class Eleccion:
+    def __init__(self): self.candidatos,self.votantes=[],[]
+    def agregar_candidato(self,c): self.candidatos.append(c)
+    def agregar_votante(self,v): self.votantes.append(v)
+    def votar(self,votante,candidato):
+        if votante.voto is None: votante.voto=candidato.nombre;candidato.votos+=1
+    def resultados(self): print({c.nombre:c.votos for c in self.candidatos})
 
-class Clase35B:
-    def __init__(self, dato):
-        self.dato = dato
-
-if __name__ == "__main__":
-    a = Clase35A("EjemploA")
-    b = Clase35B("EjemploB")
-    print(a.nombre, b.dato)
+# Ejemplo
+c1,C2= Candidato("Alice"),Candidato("Bob")
+v1,v2= Votante("Juan"),Votante("Maria")
+elec=Eleccion(); elec.agregar_candidato(c1); elec.agregar_candidato(C2)
+elec.agregar_votante(v1); elec.agregar_votante(v2)
+elec.votar(v1,c1); elec.votar(v2,C2); elec.resultados()
