862f71e1901cb8fd6742ce5ae536d9bfaa936499
DavidAnzola05 <davidanzola014@gmail.com>
2025-09-28 19:57:22 -0500

    Update ej7.py

--- ej7.py
+++ ej7.py
@@ -1,20 +1,11 @@
-class contadorTexto:
-    def __init__(self, texto):
-        self.texto = texto
+class ContadorTexto:
+    def __init__(self, texto): self.texto = texto
+    def contar_palabras(self): return len(self.texto.split())
+    def contar_caracteres(self): return len(self.texto)
+    def contar_lineas(self): return len(self.texto.splitlines())
 
-    def contar_palabras(self):
-        palabras = self.texto.split()
-        return len(palabras)
-    def contar_caracteres(self):
-        return len(self.texto)
-    def contar_lineas(self):
-        lineas = self.texto.splitlines()
-        return len(lineas)
-    
-texto = input("Ingrese un texto: ")
-contador = contadorTexto(texto)
-
+t = ContadorTexto(input("Ingrese un texto: "))
 print("El texto tiene:")
-print(f"{contador.contar_palabras()} palabras")
-print(f"{contador.contar_caracteres()} caracteres")
-print(f"{contador.contar_lineas()} lineas")
+print(f"{t.contar_palabras()} palabras")
+print(f"{t.contar_caracteres()} caracteres")
+print(f"{t.contar_lineas()} líneas")
